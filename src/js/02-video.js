918f533c65ae3a23dede37bb248271521c79267c
Bartlomiej Kokosza <bartlomiej.kokosza@gmail.com>
2023-04-24 12:46:53 +0200

    Zadanie 2

---
+++ src/js/02-video.js
@@ -0,0 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const player = new Player('vimeo-player');
+
+
+const timeCounter = throttle( event => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+    }, 1000);
+
+    //zapisuje do local storage czas, sprawdzamy go co 1s = 1000 
+
+player.on('timeupdate',  timeCounter);
+//zapisuje ilosc sekund ktore uplynely do LS
+
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
